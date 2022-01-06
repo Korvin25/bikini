05d35b88be7c10e8e18faa04c33870b90c47703a
Valentin Glinskiy <v.valych@gmail.com>
2022-01-06 05:52:17 +0300

    удалил старый апп paypal

--- apps/third_party/paypal/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-https://github.com/paypal/Checkout-Python-SDK
-(version 1.0.1)
-"""
-name = "paypalcheckoutsdk"
-from apps.third_party.paypal.config import *  # noqa
--- apps/third_party/paypal/payments/__init__.py
+++
@@ -1,7 +0,0 @@
-from apps.third_party.paypal.payments.authorizations_capture_request import *  # noqa
-from apps.third_party.paypal.payments.authorizations_get_request import *  # noqa
-from apps.third_party.paypal.payments.authorizations_reauthorize_request import *  # noqa
-from apps.third_party.paypal.payments.authorizations_void_request import *  # noqa
-from apps.third_party.paypal.payments.captures_get_request import *  # noqa
-from apps.third_party.paypal.payments.captures_refund_request import *  # noqa
-from apps.third_party.paypal.payments.refunds_get_request import *  # noqa
