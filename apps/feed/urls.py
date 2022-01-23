bd6594df73ed4645c323920f7a0e3c3ddd97980a
antonkostin <antonkostin86@gmail.com>
2022-01-23 11:42:22 +0300

    fix feed ozon

--- apps/feed/urls.py
+++ apps/feed/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^yandex.xml$', views_rss.yandex_rss, name='yandex_rss'),
     url(r'^aliexpress.xml$', views_rss.aliexpress_rss, name='aliexpress_rss'),
-    url(r'^ozon.xml$', views_rss.ozon_rss, name='ozon_rss'),
+    url(r'^ozon_update.xml$', views_rss.ozon_rss, name='ozon_rss'),
 ]
