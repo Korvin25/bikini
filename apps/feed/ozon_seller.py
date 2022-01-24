f58f329d9368b45db2ec359072e2747369e2fb30
antonkostin <antonkostin86@gmail.com>
2022-01-24 10:23:37 +0300

    fix

--- apps/feed/ozon_seller.py
+++ apps/feed/ozon_seller.py
@@ -37,7 +37,7 @@ class OzonSeller():
             lists = {
                 "items": []
             }
-
+            print(product.extra_options.all())
             colors_id = product.attrs.get('color', [])
             sizes_id = product.attrs.get('bottom_size', []) + product.attrs.get('top_size', [])  + product.attrs.get('size', []) + product.attrs.get('razmer_kupalnika', []) + product.attrs.get('size_yubka_dop', [])
             shueze_size_id =  product.attrs.get('shueze_size', []) 
