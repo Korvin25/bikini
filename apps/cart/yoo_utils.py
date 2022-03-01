9bc2b3809e21a7569306eb442bffdf46fbe58b90
antonkostin <antonkostin86@gmail.com>
2022-03-01 16:41:48 +0300

    add life pay

--- apps/cart/yoo_utils.py
+++ apps/cart/yoo_utils.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import requests
+import json
+
 from django.utils import timezone
+from django.conf import settings
 
 from ..third_party.yookassa import Payment as YooPayment
 
@@ -38,6 +42,36 @@ def yoo_update_cart_with_payment(cart, payment=None, force=False, logger=None):
             cart.send_order_emails()
             # -- остатки на складе --
             cart.update_in_stock()
+
+            try:
+                # -- Удаленная фискализация --
+                url = u'https://sapi.life-pay.ru/cloud-print/create-receipt'
+                purchase =  {
+                    "products": [
+                        {
+                            "name": item.product.title, 
+                            "price": item.product.price_rub, 
+                            "quantity": item.count
+                        } 
+                        for item in cart.cart_items
+                    ]
+                }
+
+
+                headers = {}
+                data = {
+                    'apikey': settings.LIFE_PAY_API_KEY,
+                    'login': settings.LIFE_PAY_API_LOGIN,
+                    'purchase': json.dumps(purchase),
+                    'test': 1,
+                }
+                req = requests.post(url, headers=headers, data=data)
+                if logger: logger.info('  (cart id {} / payment {}) updating cart... Удаленная фискализация: {}'.format(cart.id, payment.id, req))
+
+            except Exception as e:
+                if logger: logger.info('  (cart id {} / payment {}) updating cart... Удаленная фискализация: {}'.format(cart.id, payment.id, e))
+
+
 
         if logger: logger.info('  (cart id {} / payment {}) done! new status: {}'.format(cart.id, payment.id, cart.yoo_status))
         return cart.yoo_status
