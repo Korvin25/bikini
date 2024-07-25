d1c6a710eb7ef57e6fa9467b04a92c8b89408d14
soyus1 <soyus.s1@yandex.ru>
2024-07-25 16:51:35 +0300

    add retailcrm

---
+++ apps/feed/management/commands/save_feed_retail_crm.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import itertools
+import xml.etree.ElementTree as et
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from apps.catalog.models import AttributeOption, Product
+from apps.feed.utils import GenerateFeed
+from apps.feed.views import PARAMS
+
+
+class Command(BaseCommand):
+    help = u'Обновить и добавить товары на Озон'
+
+    def handle(self, *args, **options):
+
+        feed = GenerateFeed(**PARAMS)
+        for product in Product.objects.filter(retailcrm=True, show=True):
+            for combinations_id in itertools.product(*product.attrs.values()):
+                combinations = AttributeOption.objects.filter(pk__in=combinations_id)
+                feed.create_retailcrm_item(product, combinations)
+
+    
+        tree = et.ElementTree(feed.el_root)
+        # Запись дерева элементов в файл
+        tree.write(os.path.join(settings.STATIC_ROOT, 'retailcrm.xml'))
+
