9324e37d1bc6f5664c9f51d9a499d7dbd404bd93
soyus1 <soyus.s1@yandex.ru>
2024-07-25 20:12:57 +0300

    add retailcrm

--- apps/cart/management/commands/retailcrm_send.py
+++ apps/cart/management/commands/retailcrm_send.py
@@ -46,10 +46,10 @@ class Command(BaseCommand):
                             },
                             'properties': get_properties(item),
                             'article': get_article(item),
-                            'initialPrice': float(item.option_price_c) if item.with_wrapping else float(item.option.price), 
+                            'initialPrice': float(item.option_price_c), 
                             'productName': item.option.title,
                             'quantity': item.count,
-                            'discountManualPercent': float(item.discount),
+                            # 'discountManualPercent': float(item.discount),
                             'discountManualAmount': float((item.option_price_c * item.discount)/100)
                         }
                         for item in items
