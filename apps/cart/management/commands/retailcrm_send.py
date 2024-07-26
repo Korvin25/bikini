aef1d53e014397739fa877ecd0611583d453f5af
soyus1 <soyus.s1@yandex.ru>
2024-07-26 12:53:27 +0300

    add retailcrm

--- apps/cart/management/commands/retailcrm_send.py
+++ apps/cart/management/commands/retailcrm_send.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import retailcrm
-from uuslug import slugify
 from django.core.management.base import BaseCommand
 
 from apps.cart.models import Cart as CartModel
 from apps.cart.retailcrm_utils import send_retailcrm
-from apps.catalog.models import AttributeOption, GiftWrapping
 
 
 class Command(BaseCommand):
