1fbe8695240b062e6a8a911edd474b184aa85041
soyus1 <soyus.s1@yandex.ru>
2024-07-26 12:35:36 +0300

    add retailcrm

--- apps/cart/management/commands/retailcrm_send.py
+++ apps/cart/management/commands/retailcrm_send.py
@@ -19,68 +19,70 @@ class Command(BaseCommand):
         site = 'bikinimini'
 
         for cart in carts:
-            items = cart.cart_items
+            cart.retailcrm = 'Old Cart'
+            cart.save()
+            # items = cart.cart_items
 
-            if items:
-                order = {
-                    'orderMethod': 'shopping-cart',
-                    'firstName': cart.profile.name,
-                    'phone': cart.profile.phone,
-                    'email': cart.profile.email,
-                    'createdAt': cart.creation_date.strftime('%Y-%m-%d %H:%M:%S'),
-                    'status': get_status(cart.payment_status),
-                    'delivery': {
-                        'cost': float(cart.delivery_method.price_rub),
-                        'code': cart.delivery_method.code_retailcrm,
-                        'address': {
-                            'index': cart.postal_code,
-                            'countryIso': cart.country.title,
-                            'city': cart.city,
-                            'street': cart.address,
-                        }
-                    },
-                    'payments': [{
-                        'amount': float(cart.summary_c),
-                        'type': cart.payment_method.code_retailcrm,
-                        'status': get_status_payments(cart.payment_status),
-                    }],
-                    'items': [
-                        {
-                            'offer': {
-                                'externalId': get_article(item),
-                            },
-                            'properties': get_properties(item),
-                            'article': get_article(item),
-                            'initialPrice': float(item.option_price_c),
-                            'productName': item.option.title,
-                            'quantity': item.count,
-                            'discountManualAmount': float((item.option_price_c * item.discount)/100),
-                        }
-                        for item in items
-                    ]
-                }
+            # if items:
+            #     order = {
+            #         'orderMethod': 'shopping-cart',
+            #         'firstName': cart.profile.name,
+            #         'phone': cart.profile.phone,
+            #         'email': cart.profile.email,
+            #         'createdAt': cart.creation_date.strftime('%Y-%m-%d %H:%M:%S'),
+            #         'status': get_status(cart.payment_status),
+            #         'delivery': {
+            #             'cost': float(cart.delivery_method.price_rub),
+            #             'code': cart.delivery_method.code_retailcrm,
+            #             'address': {
+            #                 'index': cart.postal_code,
+            #                 'countryIso': cart.country.title,
+            #                 'city': cart.city,
+            #                 'street': cart.address,
+            #             }
+            #         },
+            #         'payments': [{
+            #             'amount': float(cart.summary_c),
+            #             'type': cart.payment_method.code_retailcrm,
+            #             'status': get_status_payments(cart.payment_status),
+            #         }],
+            #         'items': [
+            #             {
+            #                 'offer': {
+            #                     'externalId': get_article(item),
+            #                 },
+            #                 'properties': get_properties(item),
+            #                 'article': get_article(item),
+            #                 'initialPrice': float(item.option_price_c),
+            #                 'productName': item.option.title,
+            #                 'quantity': item.count,
+            #                 'discountManualAmount': float((item.option_price_c * item.discount)/100),
+            #             }
+            #             for item in items
+            #         ]
+            #     }
 
-                with_wrapping = {
-                    'initialPrice': float(GiftWrapping.objects.first().price_rub),
-                    'productName': u'Подарочная упаковка',
-                    'quantity': 0,
-                }
+            #     with_wrapping = {
+            #         'initialPrice': float(GiftWrapping.objects.first().price_rub),
+            #         'productName': u'Подарочная упаковка',
+            #         'quantity': 0,
+            #     }
 
-                for item in items:
-                    if item.with_wrapping:
-                        with_wrapping['quantity'] += 1
+            #     for item in items:
+            #         if item.with_wrapping:
+            #             with_wrapping['quantity'] += 1
 
-                if with_wrapping['quantity'] > 0:
-                    order['items'].append(with_wrapping)
+            #     if with_wrapping['quantity'] > 0:
+            #         order['items'].append(with_wrapping)
 
 
-                result = client.order_create(order, site)
-                print(result.get_response())
-                if result.is_successful():
-                    retailcrm_id = result.get_response()['id']
-                    print(retailcrm_id)
-                    cart.retailcrm = retailcrm_id
-                    cart.save()
+            #     result = client.order_create(order, site)
+            #     print(result.get_response())
+            #     if result.is_successful():
+            #         retailcrm_id = result.get_response()['id']
+            #         print(retailcrm_id)
+            #         cart.retailcrm = retailcrm_id
+            #         cart.save()
 
 
 def get_status(status):
